ded2ef36a5d0cc63606b2d378c1d3fe48c8c01ab
aaarchmiller <althea.archmiller@gmail.com>
2016-10-27 13:06:35 -0500

    error occurred when installing library that was already loaded; restart fixed

--- R/uhcsimXvalid.R
+++ R/uhcsimXvalid.R
@@ -14,8 +14,11 @@
 #' @param navail The number of available locations in the data set (ie not used)
 #' @param ncovariates The number of covariates or p in the model
 #' @param model_form The model in the form: "y~x1+x2+...+xp" (including quotations)
-#' @param z_colnames The column names of the environmental characteristics to be plotted with \code{uhcdensplot}. Takes the form: c("x1","x2",...,"xp")
-#' @param xmat_colnames The column names of the environmental characteristics that were used as predictors in the model_form. Takes the form: c("x1","x2",...,"xp")
+#' @param z_colnames The column names of the environmental characteristics to
+#' be plotted with \code{uhcdensplot}. Takes the form: c("x1","x2",...,"xp")
+#' @param xmat_colnames The column names of the environmental characteristics
+#' that were used as predictors in the model_form. Takes the form:
+#' c("x1","x2",...,"xp")
 #'
 #' @return An array of dimensions nsims x nused_test x p.
 #'
