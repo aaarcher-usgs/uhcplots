29a68b352f0ee8461429f64d262cddc78aaea441
aaarchmiller <althea.archmiller@gmail.com>
2016-10-28 11:18:18 -0500

    added content/formatting for packageDescription file

--- R/uhcplots.R
+++ R/uhcplots.R
@@ -3,11 +3,24 @@
 #'   packageVersion remove.packages
 NULL
 
-#' Package development tools for R.
-#' # Used-Habitat Calibration Plots (UHC Plots)
+#' Used-Habitat Calibration Plots (UHC Plots)
+#'
+#' @section Details:
+#'
+#' Used-Habitat Calibration plots are a new method for model calibration that
+#' can be applied across a spectrum of species distribution models (SDMs)
+#' (Fieberg et al. In revision for Ecology Letters).
 #'
 #' @section See also:
 #'
-#' Fieberg, et al. (In Revision)
+#'
+#' Fieberg, Forester, Street, Johnson, ArchMiller, and Matthiopoulos.
+#' In Revision. Species distribution models: predictive snipers or shots in the
+#' dark? Ecology Letters.
+#'
+#' Full code to reproduce the paper at: <http://doi.org/10.13020/D6T590>
+#'
+#'
+#' @docType package
 #' @name uhcplots
 NULL
