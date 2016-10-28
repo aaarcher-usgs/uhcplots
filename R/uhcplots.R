a881c251544e939edb916839b16a71c761cfa870
aaarchmiller <althea.archmiller@gmail.com>
2016-10-28 11:07:18 -0500

    Creating packageDescription file

---
+++ R/uhcplots.R
@@ -0,0 +1,13 @@
+#' @importFrom utils available.packages contrib.url install.packages
+#'   installed.packages menu modifyList packageDescription
+#'   packageVersion remove.packages
+NULL
+
+#' Package development tools for R.
+#' # Used-Habitat Calibration Plots (UHC Plots)
+#'
+#' @section See also:
+#'
+#' Fieberg, et al. (In Revision)
+#' @name uhcplots
+NULL
