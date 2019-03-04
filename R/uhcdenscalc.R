22ca7882e92cd5ffda0f463a3c34af3ce0899930
jfieberg <jfieberg@umn.edu>
2019-03-04 17:53:37 +0000

    fixed warning message when using uhcdenscalc.R and updated the vignette to contain the correct citation for the paper.

--- R/uhcdenscalc.R
+++ R/uhcdenscalc.R
@@ -85,13 +85,13 @@ uhcdenscalc <- function(rand_sims, dat, avail, gridsize=500){
   # to hold density estimates fU^(z) for all nsims datasets
   densrand <- matrix(NA, nsims,gridsize)
   # observed data density, fu(z)
-  densdat <- KernSmooth::bkde(dat, range.x=range.x, gridsize=gridsize)
+  densdat <- KernSmooth::bkde(as.vector(dat), range.x=range.x, gridsize=gridsize)
   for(i in 1:nsims){ # densities for simulated data sets
     temp <- KernSmooth::bkde(rand_sims[i,], range.x=range.x, gridsize=gridsize)
     densrand[i,] <- temp$y #estimated density
   }
   # density of available points
-  densavail <- KernSmooth::bkde(avail,range.x=range.x, gridsize=gridsize)
+  densavail <- KernSmooth::bkde(as.vector(avail),range.x=range.x, gridsize=gridsize)
   out <- list(densdat=densdat, densrand=densrand, densavail=densavail)
   return(out)
 }
